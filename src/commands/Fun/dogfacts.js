e2bed75045de1f0922ac0114ae5e38a844a2b2f9
Shiro <53873873+Shirodork@users.noreply.github.com>
2021-03-08 11:41:13 -0800

    New Commands

---
+++ src/commands/Fun/dogfacts.js
@@ -0,0 +1,30 @@
+const dogFacts = require('../Assets/dogfacts.json');
+const Command = require('../../structures/Command.js');
+
+module.exports = class DogFacts extends Command {
+	constructor(bot) {
+		super(bot, {
+			name: 'dogfacts',
+			dirname: __dirname,
+            botPermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+            aliases: ['dogfact', 'dogf'],
+			description: 'Grab a random dog fact!',
+			usage: 'dogfacts',
+			cooldown: 1000,
+		});
+	}
+
+	// Run command
+	async run(bot, message, args, settings) {
+
+        // New Embed
+			const embed = new MessageEmbed()										
+            .setTitle(message.translate(settings.Language, 'FUN/DOG_TITLE'))	// Title
+            .setColor('RANDOM')													// Color
+            .setDescription(dogFacts[Math.floor(Math.random() * dogFacts.length)])									// Grab Chosen Fact
+        
+        // Send Message
+        message.channel.send(embed);
+    
+	}
+};
